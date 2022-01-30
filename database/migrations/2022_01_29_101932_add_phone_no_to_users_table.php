ab69f000806017ac7ef41ea4e960d42e691daa8c
Abraham Maleko <abrahammaleko@gmail.com>
2022-01-30 17:37:59 +0300

    added phone no to users table

--- database/migrations/2022_01_29_101932_add_phone_no_to_users_table.php
+++ database/migrations/2022_01_29_101932_add_phone_no_to_users_table.php
@@ -14,7 +14,7 @@ class AddPhoneNoToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+          $table->string('phone_no');
         });
     }
 
